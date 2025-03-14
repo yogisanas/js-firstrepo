7429c2330ce67f6f1e43c4f46f9d98492ecac2d0
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-03-14 13:11:46 +0000

    changes done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,30 @@
 "use strict";  //--treat all JS code as newer version or newer JS
 
-alert(3+3);
+//alert(3+3);   //--we are using nodejs, not browser
+
+console.log(3 
+    +
+     3);    //code readability should be high
+
+
+let name = "yogita"
+let age = 18
+let isLoggedIn = false
+let state;         //--undefined
+let state1 = "";  //--null
+
+// ----primitive_datatypes-------
+// -number = 2 to power 53
+// -bigint
+// -string = ""
+// -boolean  = truse/false
+// -null = standalone value
+// -undefined = declared but not initialize
+// -symbol = unique
+
+
+// -object
+
+console.log(typeof undefined);     //--undefined
+console.log(typeof null);         //--object
 
