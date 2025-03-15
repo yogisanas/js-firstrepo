ed2a4697afb34a5218895c3a866298d0eb29616e
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-03-15 10:11:25 +0000

    Conversion of DataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,35 @@
-let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "yogita"
 
-console.log(typeof score);
-console.log(typeof(score));
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" = 33
+// "33abc" = NaN
+//true = 1; false = 0
+
+let isLoggedIn = "yogii"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false
+// "yogi" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
 
