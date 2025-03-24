c86f320b1faaf9c0cf11fbbf2f5837b6cc3f2440
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-03-24 14:24:33 +0000

    done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,22 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)   //--object
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));        //--specially e-commerce website
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());   //--us-standards
+// console.log(hundreds.toLocaleString('en-In'));  //--indian-standards
+
+// +++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
