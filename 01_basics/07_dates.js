2da5cace36941e92fd921fb7cfad6a05b8390d95
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-03-25 07:24:41 +0000

    done

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -25,5 +25,15 @@ let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate3.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
-console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDate()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
