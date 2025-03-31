3114e40730bbaa1a7485e5d5d91af0f689d1c647
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-03-31 20:03:50 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,8 +2,14 @@
 // object.create
 
 //-object Literals
+
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Yogita",
+    "full name": "yogita sanas",
+    // mySym: "mykey1",
+    [mySym]: "mykey1",
     age: 20,
     location: "Mumbai",
     email: "yogita@gmail.com",
@@ -13,3 +19,7 @@ const JsUser = {
 
 console.log(JsUser.name);
 console.log(JsUser["name"]);
+console.log(JsUser["full name"]);
+// console.log(JsUser.mySym);
+// console.log(typeof JsUser.mySym);   //--string
+console.log(JsUser[mySym]);
