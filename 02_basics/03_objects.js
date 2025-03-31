2471824e61a821d05b5033f7c8c7bc541fba7ebf
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-03-31 12:04:14 +0000

    done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+//-singleton
+// object.create
+
+//-object Literals
+const JsUser = {
+    name: "Yogita",
+    age: 20,
+    location: "Mumbai",
+    email: "yogita@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.name);
+console.log(JsUser["name"]);
