b0e2d9251f06cfb7d7f363ed79201b544046ce38
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-04-04 12:41:34 +0000

    done

---
+++ Array/array_impThings.js
@@ -0,0 +1,15 @@
+//--foreach, map, filter, find, indexOf
+
+var arr = [1, 2, 3, 4]
+
+// arr.forEach(function(val){
+//     console.log(val + " Hello");   
+// })
+
+//--map - It returns a new array with memory according to mapped array.
+
+var newarr = arr.map(function(val){
+    return 13;       //--return val;
+})
+
+console.log(newarr);
