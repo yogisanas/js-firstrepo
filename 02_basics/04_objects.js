5c0142c0689a7caded9039a8a7287c184f526129
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-05-07 02:21:05 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,4 +5,28 @@ tinderUser.id = "123abc"
 tinderUser.name = "yogii"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email : "yogii@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname : "yogita",
+            lastname : "sanas"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname);
+
+// console.log(regularUser.fullname?.userfullname);  //--use ? to check if fullname is present or not 
+                                                     // mostly for api
+
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {2 : "a", 4 : "b"}
+
+const obj3 = { obj1, obj2}
+console.log(obj3);
+
