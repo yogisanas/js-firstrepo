f14946e7903cf865f9df54943dfa13b7131ded79
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-05-29 17:23:27 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -7,7 +7,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "yogii"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+// console.log(tinderUser);   //--{ id: '123abc', name: 'yogii', isLoggedIn: false }
 
 
 const regularUser = {
@@ -27,8 +27,8 @@ console.log(regularUser.fullname.userfullname);
 
 
 const obj1 = {1 : "a", 2 : "b"}
-const obj2 = {2 : "a", 4 : "b"}
+const obj2 = {3 : "a", 4 : "b"}
 
-const obj3 = { obj1, obj2}
-console.log(obj3);
+const obj3 = { obj1, obj2}    //--add both obj on new obj
+console.log(obj3);           //--{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 
