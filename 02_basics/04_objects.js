8a36b10324d0e5f670df8b267a65fd5361c5b631
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-04 12:27:54 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,6 +29,25 @@ console.log(regularUser.fullname.userfullname);
 const obj1 = {1 : "a", 2 : "b"}
 const obj2 = {3 : "a", 4 : "b"}
 
+const obj5 = {5 : "a", 6 : "b"}
+
 const obj3 = { obj1, obj2}    //--add both obj in new obj
 console.log(obj3);           //--{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+const obj4 = Object.assign(obj1, obj2)  //--({}, obj1, obj2)  --all value combine in that parameter
+console.log(obj4);          //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+console.log(obj1);          //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' } --it consider obj1 as target
+
+const allObj = Object.assign({}, obj1, obj2, obj5)
+console.log(allObj);
+console.log(obj1);
+
+//--IMP SYNTAX:-
+const obj6 = {...obj1, ...obj2}
+console.log(obj6);       //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }  --commonly use syntax......
+
+
+
+
+
 
