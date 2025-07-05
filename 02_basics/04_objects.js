80bd6d70e94a888fb095587f55106b544e3c7e9e
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-05 14:48:19 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,7 +1,7 @@
 // const tinderUser = {}
 const tinderUser = new Object()   
 
-console.log(tinderUser);      //--{}
+// console.log(tinderUser);      //--{}
 
 tinderUser.id = "123abc"
 tinderUser.name = "yogii"
@@ -20,7 +20,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname);
 
 // console.log(regularUser.fullname?.userfullname);  //--use ? to check if fullname is present or not 
                                                      // mostly for api
@@ -32,22 +32,41 @@ const obj2 = {3 : "a", 4 : "b"}
 const obj5 = {5 : "a", 6 : "b"}
 
 const obj3 = { obj1, obj2}    //--add both obj in new obj
-console.log(obj3);           //--{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+// console.log(obj3);           //--{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 
 const obj4 = Object.assign(obj1, obj2)  //--({}, obj1, obj2)  --all value combine in that parameter
-console.log(obj4);          //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
-console.log(obj1);          //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' } --it consider obj1 as target
+// console.log(obj4);          //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// console.log(obj1);          //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' } --it consider obj1 as target
 
 const allObj = Object.assign({}, obj1, obj2, obj5)
-console.log(allObj);
-console.log(obj1);
+// console.log(allObj);
+// console.log(obj1);
 
 //--IMP SYNTAX:-
 const obj6 = {...obj1, ...obj2}
-console.log(obj6);       //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }  --commonly use syntax......
+// console.log(obj6);       //--{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }  --commonly use syntax......
 
 
+//-- whenever data fetch from database, it looks like this...
+const users = [ 
+    {
+        id : 1,
+        name : "yogii"
+    },
+    {
+        id : 2,
+        name : "riya"
+    }
+]
 
+users[1].name
+console.log(tinderUser);
 
+console.log(Object.keys(tinderUser));     //--[ 'id', 'name', 'isLoggedIn' ]   --data type is array
+console.log(Object.values(tinderUser));     //--[ '123abc', 'yogii', false ]   --data type is array
+console.log(Object.entries(tinderUser));     //--[ [ 'id', '123abc' ], [ 'name', 'yogii' ], [ 'isLoggedIn', false ] ]   --data type is array
 
+console.log(tinderUser.hasOwnProperty('isLogged'));   //--false     --check that property is available or not
 
+//--For more methods of objects visit browser & inspect 
+//--      const obj1 = {1 : "a", 2 : "b"}
