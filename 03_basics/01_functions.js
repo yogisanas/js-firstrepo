b16e91dad4b79a60b6e1c139ad86e81d964a4d75
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-10 15:36:09 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,7 +14,35 @@ function addTwoNumbers(num1, num2){
     console.log(num1 + num2);
 }
 
-addTwoNumbers(10, 10)  //--20
-addTwoNumbers(10, "10")  //--1010
-addTwoNumbers(10, "a")  //--10a
-addTwoNumbers(10, null)  //--10
+// addTwoNumbers()        //--NaN
+// addTwoNumbers(10, 10)  //--20
+// addTwoNumbers(10, "10")  //--1010
+// addTwoNumbers(10, "a")  //--10a
+// addTwoNumbers(10, null)  //--10
+
+const result = addTwoNumbers(10, 10)
+console.log("Result: ", result);      //--Result:  undefined
+//--cannot store result in variable because that function is not return anything..
+
+
+function subTwoNumbers(num1, num2){
+    let result = num1 - num2
+    // console.log("yogii");   
+    return result
+    // return num1 + num2         //--for saving memory
+}
+
+const result2 = subTwoNumbers(20, 10)
+console.log("Result: ", result2);      //--10
+
+
+function loginUserMessage(username){            //--(username = "yogii")  --default value/override value
+    if(username === undefined){                    //--if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("yogita"));   //--yogita just logged in
+console.log(loginUserMessage());           //--undefined just logged in
