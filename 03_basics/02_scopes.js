dcaab8949e9568fd1321f1ee18d6af6ed6e26b7f
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-12 13:23:44 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,18 @@
-let a = 10
-const b = 20
-var c = 30
+// var c = 300      //--now the value of c is 30 because of override
+let a = 300      //--global variable
 
-console.log(a);
-console.log(b);
-console.log(c);
+if(true){
+   let a = 10     //--local variable
+   const b = 20
+   var c = 30    //--c = 30
+
+   console.log(a);   //--10
+}
+
+// console.log(a);    //-- a is not defined
+// console.log(b);    //--b is not defined
+// console.log(c);     //--30
+
+console.log(a);     //--300
+
+
