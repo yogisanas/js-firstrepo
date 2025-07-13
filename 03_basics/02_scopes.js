8d51d696ad218a2143b6e28516903127031ec3f2
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-13 09:03:14 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,13 +6,57 @@ if(true){
    const b = 20
    var c = 30    //--c = 30
 
-   console.log(a);   //--10
+   // console.log(a);   //--10
 }
 
 // console.log(a);    //-- a is not defined
 // console.log(b);    //--b is not defined
 // console.log(c);     //--30
 
-console.log(a);     //--300
+// console.log(a);     //--300
 
 
+function one(){
+   const username = "yogita"
+
+   function two(){
+      const website = "youtube"
+      console.log(username);
+   }
+   // console.log(website);      //--website is not defined
+   
+   two()
+}
+
+// one()        //--yogita   --method one is calling method two 
+
+
+if(true){
+   const username = "yogita"
+   if (username === "yogita") {
+      const website = " youtube"
+      // console.log(username + website);
+   }
+   // console.log(website);      //--website is not defined
+}
+
+// console.log(username);       //--username is not defined
+
+
+
+// ************************ Interesting *******************************************
+
+console.log(addOne(5));     //--6
+
+function addOne(num){
+   return num + 1
+}
+
+addOne(5)
+
+addTwo(5)          //--Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+   return num + 2
+}
+
+addTwo(5)
