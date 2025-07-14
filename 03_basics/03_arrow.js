4b1692c60561032dffe5695a78c8bdf390d6b5fa
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-14 01:49:08 +0000

    done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,4 +1,31 @@
-const user = {
+//--context --> values hold by variables
+
+const user = {                //--current context   --inside scope
     username: "yogita",
-    
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
 }
+
+user.welcomeMessage()    //--yogita , welcome to website
+user.username = "sam"   //--change context
+user.welcomeMessage()    //--sam , welcome to website
+
+//-------------- Output--------------------------
+// yogita , welcome to website
+// {
+//   username: 'yogita',
+//   price: 999,
+//   welcomeMessage: [Function: welcomeMessage]
+// }
+// sam , welcome to website
+// {
+//   username: 'sam',
+//   price: 999,
+//   welcomeMessage: [Function: welcomeMessage]
+// }
+
+console.log(this);    //--{}   --because of node environment
