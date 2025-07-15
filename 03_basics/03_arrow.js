c45c71454915c83e642b557824c93d08fd889836
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-15 10:09:02 +0000

    done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -34,9 +34,62 @@ const user = {                //--current context   --inside scope
 
 // ************************* Arrow Function *************************************
 
-function chai(){
+// function chai(){
+//     let username = "yogita"
+//     console.log(this);             //--object
+//     console.log(this.username);      //--undefined
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let username = "yogita"
+//     console.log(this.username);     //--undefined
+// }
+
+// *****************************************************************************************
+
+// () => {}       //--syntax of arrow function
+
+const chai = () => {
     let username = "yogita"
-    console.log(this);
+    console.log(this.username);     //--undefined
+    console.log(this);            //--{}   --not work in arrow function
 }
 
-chai()
+// chai()
+
+
+// ----Basic arrow function-------------------------------------------------------------
+
+// console.log(addTwo(2, 2));    //--Cannot access 'addTwo' before initialization
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addTwo(2, 2));    //--4
+
+
+//----Implicit return--------------------------------------------------------------------
+//--Implicit return means they can understand no need to write return keyword.
+//--Explicit return means return keyword is required.
+//--{} --return keyword is required.
+//--() --no need to write return keyword.
+
+// const addTwo = (num1, num2) =>  num1 + num2         //--2
+
+// const addTwo = (num1, num2) =>  ( num1 + num2 )       //--4
+
+// const addTwo = (num1, num2) =>  {username: "yogita"}    //--undefined
+
+const addTwo = (num1, num2) =>  ({username: "yogita"})    //--{ username: 'yogita' }
+
+console.log(addTwo(2, 2));    //--4
+
+//--Ex.------
+// const myArray = [1, 2, 3, 4, 5]
+
+// myArray.forEach(function() {})
+
+// myArray.forEach(() => {})
