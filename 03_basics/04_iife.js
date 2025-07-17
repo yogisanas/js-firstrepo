e12f351d5eac4d85121774b2b2ed5831df490892
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-17 15:24:55 +0000

    done

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,18 @@
-//-- Immediately Invoked Function Expression (IIFE)
+//*** Immediately Invoked Function Expressions (IIFE) **************************
+//--Two reasons that we need IIFE.
+//--1. Don't want global pollution.
+//--2. Immediately invoked function.
 
+//--Named IIFE------------------
+(function chai(){
+    console.log("DB CONNECTED");
+})();                              
+// chai()
 
+//--to end 1st invoked function & to start 2nd invoked function use (;)
 
+//--Unnamed / Simple IIFE------------
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('yogita')
 
