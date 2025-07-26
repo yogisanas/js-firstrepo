c36dad342d07cb6eb91d6f304643842439b42215
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-26 19:44:42 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,3 +1,35 @@
-if(true){
+//-- if
+//  <, >, <=, >=, ==, !=, ===, !==(strict checking)
 
-}    
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature < 50){
+//     console.log("Less than 50");
+// }    
+// else{
+//     console.log("Greater thn 50");
+// }
+
+
+// const score = 200
+
+// if(score >  100){
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);    //--User Power: fly
+// }
+
+//     console.log(`User Power: ${power}`);   //--power is not defined
+
+
+// ******* SHORT HAND NOTATION *****************
+//--Implicit scope
+
+const balance = 1000
+
+if(balance > 500) console.log("Test");          //--for one line
+
+// if(balance > 500) console.log("Test"),      //--for multiple line / not good
+// console.log("Test2");                
+
+
