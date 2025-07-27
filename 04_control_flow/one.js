a6db29c88a137b24ed04eb595b4670af3d8dd238
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-27 19:28:24 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -22,14 +22,41 @@
 //     console.log(`User Power: ${power}`);   //--power is not defined
 
 
-// ******* SHORT HAND NOTATION *****************
+// ******* SHORT HAND NOTATION *******************************************************************
 //--Implicit scope
 
-const balance = 1000
+// const balance = 1000
 
-if(balance > 500) console.log("Test");          //--for one line
+// if(balance > 500) console.log("Test");          //--for one line
 
 // if(balance > 500) console.log("Test"),      //--for multiple line / not good
 // console.log("Test2");                
 
 
+// ******* Nesting *******************************************************************
+
+// if (balance < 500) {
+//     console.log("Less than 500");
+// } else if (balance < 700) {
+//     console.log("Less than 700");
+// }
+// else if(balance < 900) {
+//     console.log("Less than 900");
+// } else {
+//     console.log("Less than 1200");
+// }
+
+// ******* EX. *******************************************************************
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || LoggedInFromEmail) {
+    console.log("User logged in");
+}
