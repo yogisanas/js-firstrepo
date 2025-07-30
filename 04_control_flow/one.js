516846bf86115ee7ab526d5b853b85b374930890
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-07-30 05:12:44 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -51,12 +51,12 @@
 const userLoggedIn = true
 const debitCard = true
 const loggedInFromGoogle = false
-const LoggedInFromEmail = true
+const loggedInFromEmail = true
 
 if (userLoggedIn && debitCard && 2==2) {
     console.log("Allow to buy course");
 }
 
-if (loggedInFromGoogle || LoggedInFromEmail) {
+if (loggedInFromGoogle || loggedInFromEmail) {
     console.log("User logged in");
 }
