0ebf644c327f2bc72077d56af42648b715ede5ea
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-08-13 17:34:16 +0000

    done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -3,9 +3,32 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if (element == 5) {
-        console.log("5 is best number");
+        // console.log("5 is best number");
     }
-    console.log(element);
+    // console.log(element);
 }
 
 // console.log(element);      //--element is not defined
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + '*' + j + ' = ' + i*j);
+        
+    }
+    
+}
+
+//-- In JS array don't have ArrayIndexOutOfBoundsExceptions it gives 'undefined'.
+
+let myArray = ["Red", "Pink", "Blue"]
+
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
