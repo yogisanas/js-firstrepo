d9d4088c93c8fa3608ff4346647982e4c28075eb
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-08-14 14:34:56 +0000

    done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -29,6 +29,27 @@ console.log(myArray.length);
 
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
     
 }
+
+
+//-- break and continue
+
+for (let i = 0; i <= 20; i++) {
+    if(i == 5)
+    {
+        console.log(`Detected 5`);
+        break;
+    }
+    console.log(`Value of i is ${i}`);  
+}
+
+for (let i = 0; i <= 20; i++) {
+    if(i == 5)
+    {
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value of i is ${i}`);    
+}       
