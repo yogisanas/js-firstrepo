e9b7e1f3df0487649497c1b0c53d45c9cb88b69f
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2025-11-05 21:26:18 +0000

    done

---
+++ 05_iterations/seven.js
@@ -0,0 +1,16 @@
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map( (item) => { return item + 10})
+const newNums = myNumbers.map( (item) => item + 10)
+// console.log(newNums);
+
+
+// const newNums2 = []
+
+// myNumbers.filter( (item) => {
+// //    item = item + 10;
+//     newNums2.push(item + 10)
+// })
+
+// console.log(newNums2);
