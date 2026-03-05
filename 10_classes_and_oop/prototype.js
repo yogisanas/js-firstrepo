8e164ad40fbdc5f31c7f933fba96628ff5d4b5cf
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2026-03-05 09:59:54 +0000

    done

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -35,3 +35,44 @@ Array.prototype.heyYogita = function(){
 
 myHeros.heyYogita();      //--Yogita say hello
 // heroPower.heyYogita();    //--heroPower.heyYogita is not a function
+
+//--------------------------------------------------------------------------------------
+
+//**** Inheritance */
+
+const User = {
+    name : "Yogii",
+    email : "yogi@gmail.com"
+}
+
+const Teacher = {
+    makeVideo : true
+}
+
+const TeachingSupport = {
+    isAvailable : false
+}
+
+const TASupport = {
+    makeAssignment : 'JS assignment',
+    fullTime : true,
+    __proto__ : TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+//-- Morden syntax:-
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+//----------------------------------------------------------------------------------------
+
+let anotherUserName = "ChaiAurCode   "
+
+String.prototype.trueLength = function(){
+    console.log(this);
+    console.log(`True length is: ${this.trim(). length}`);
+}
+
+anotherUserName.trueLength();
+"yogita".trueLength();
+"hello".trueLength();
