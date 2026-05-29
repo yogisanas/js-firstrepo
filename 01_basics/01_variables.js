183e3a2e8e6c74dc50623e4ba616e2f6ad4ecff1
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2026-05-29 18:25:10 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,11 @@
+//-Constants:-
 const accountId = 144
+
+//-Variables:-
 let accountEmail = "yogita@gmail.com"
 var accountPassword = "12345"
 accountCity = "Mumbai"
-let accountState;
+let accountState;           //--undefined   --for future initialization
 
 // accountId = 200   //--not allowed
 
@@ -18,4 +21,5 @@ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 /*
 -Prefer not to use var
 -because of issue in block scope and functional scope
+-{ scope }
 */
