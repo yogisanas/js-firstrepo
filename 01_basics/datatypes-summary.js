6afb8a55ac88789dafb895085f9f4d1f38b46fbd
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2026-07-01 03:01:28 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,8 +1,14 @@
-// Primitive (call by value)
+//  --Data types divides/categorized into two parts i.e. Primitive & Non-Primitive based on how to store data 
+//       in memory & how to access data.
+
+//--JS is dynamically typed language.
+// const score : Number = 100     //--In TypeScript   --type safety
+
+
+//#. Primitive (call by value)  --copy
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt 
 
-// const score : Number = 100     //--In TypeScript
 
 const score = 100
 const scoreValue = 100.3
@@ -13,13 +19,12 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-
-// console.log(id === anotherId);
+// console.log(id === anotherId);    --false   --return value not same
 
 const bigNumber = 5567987234569876656n
 
 
-// Non-Primitive (call by reference)
+//#. Non-Primitive / Reference Type (call by reference)   --reference
 
 // Array, Objects, Functions
 
