098502c0408d4d96671a1452c3f3e768fee2e478
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2026-07-15 20:36:19 +0000

    var

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,19 +1,22 @@
 //-Constants:-
 const accountId = 144
 
+// accountId = 200   //--not allowed
+
+// console.log(accountId);
+
+
 //-Variables:-
 let accountEmail = "yogita@gmail.com"
 var accountPassword = "12345"
 accountCity = "Mumbai"
 let accountState;           //--undefined   --for future initialization
 
-// accountId = 200   //--not allowed
 
 accountEmail = "ys@gmail.com"
 accountPassword = "212121"
 accountCity = "Pune"
 
-// console.log(accountId);
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
