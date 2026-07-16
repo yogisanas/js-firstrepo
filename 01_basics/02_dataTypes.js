5f6651504f1a29a66e7eb26bd0c65118cd5f25b7
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2026-07-16 11:46:05 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,5 @@
 "use strict";  //--treat all JS code as newer version or newer JS
+//--now no need to write this / default strict mode
 
 //alert(3+3);   //--we are using nodejs, not browser
 //--there is different syntax to use alert in nodejs
@@ -19,7 +20,7 @@ let state2 = null;  //--empty
 
 
 // ----primitive_datatypes-------
-// -number = 2 to power 53
+// -number = 2 to power 53    -range
 // -bigint
 // -string = ""
 // -boolean  = truse/false
@@ -31,5 +32,6 @@ let state2 = null;  //--empty
 
 console.log(typeof age);
 console.log(typeof "yogita");
+console.log(typeof yogita);       //--undefined
 console.log(typeof undefined);     //--undefined type
 console.log(typeof null);         //--object
