914236c11f0e594403f5fd3b5eeb1809e92be5e0
Yogita Sanas <83511891+yogisanas@users.noreply.github.com>
2026-08-01 14:45:06 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,13 +1,13 @@
 //-singleton
 // object.create    --constructor through object
 
-//-object Literals:-
+//-object Literals:-   Non-singleton
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1")     //--DT
 
 const JsUser = {
     name: "Yogita",
-    "full name": "yogita sanas",    //--can't access by dot
+    "full name": "yogita sanas",    //--can't access by dot because of 2-diff names
     // mySym: "mykey1",    //--not allowed
     [mySym]: "mykey1",
     age: 20,
@@ -17,23 +17,24 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.name);
-// console.log(JsUser["name"]);
+// console.log(JsUser.name);        //--Yogita
+// console.log(JsUser[name]);       //--ERROR - all consider as string
+// console.log(JsUser["name"]);     //--Yogita
 // console.log(JsUser["full name"]);
 // console.log(JsUser.mySym);   //--mykey1
 // console.log(typeof JsUser.mySym);   //--string
-// console.log(JsUser[mySym]);   //--string
+// console.log(JsUser[mySym]);   //--mykey1
 // console.log(typeof mySym);    //--symbol
 
 
 // JsUser.age = 25        //--override the value
-// console.log(JsUser["age"]);
+// console.log(JsUser["age"]);    //--25
 
 // Object.freeze(JsUser)    //--after freeze we can't add anything 
 // JsUser.location = "Pune"
-// console.log(JsUser["location"]);
+// console.log(JsUser["location"]);     //--Mumbai
 
-// console.log(JsUser);
+// console.log(JsUser);    //--symbol
 
 
 JsUser.greeting = function(){
@@ -47,6 +48,5 @@ JsUser.greetingTwo = function(){
 // console.log(JsUser.greeting);      //--[Function (anonymous)]  --It doesn't execute function 
                                                                 //--just return reference of that func.
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
-
+console.log(JsUser.greeting());      //--Hello....
+console.log(JsUser.greetingTwo());   //--Hello....Yogita
